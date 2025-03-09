89650167248e1ccacf4b648dbca2507a2aaf459c
Raghul-nxtwave <raghulrengasamy05@gmail.com>
2025-03-09 11:05:39 +0530

    Portfolio

---
+++ portfolio.js
@@ -0,0 +1,17 @@
+function open_menu() {
+    let nav_list = document.querySelector('.nav-list');
+    let menu_btn = document.querySelector('.menu-btn');
+
+    nav_list.classList.toggle('active');
+    menu_btn.classList.toggle('fa-bars');
+    menu_btn.classList.toggle('fa-times');
+}
+
+function open_menu() {
+    let nav_list = document.querySelector('.nav-list');
+    let menu_btn = document.querySelector('.menu-btn');
+
+    nav_list.classList.toggle('active');
+    menu_btn.classList.toggle('fa-bars');
+    menu_btn.classList.toggle('fa-times');
+}
